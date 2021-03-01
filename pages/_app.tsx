75e27086ebc11e23a28328e5884b83bacaf87ec3
appare45 <kaibatsu35.7m45@gmail.com>
2021-03-01 13:26:52 +0900

    init firebase project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import type { AppProps /*, AppContext */ } from 'next/app'
-import '../styles/globals.css'
+import type { AppProps /*, AppContext */ } from 'next/app';
+import '../styles/globals.css';
+import firebase from 'firebase/app';
+import { useEffect } from 'react';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebase.com`,
+  databaseUrl: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

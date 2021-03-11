41b5192e052762e5de041470306d5a21d73b240f
appare45 <kaibatsu35.7m45@gmail.com>
2021-03-12 02:05:35 +0900

    fix: input bug

--- components/DigitInput.tsx
+++ components/DigitInput.tsx
@@ -11,13 +11,23 @@ function DigitInput(props: { focus?: boolean; onKeyUp: Function }) {
       console.log(props.focus);
     }
   });
+  function keyUp(e: KeyboardEvent) {
+    if (
+      (e.keyCode <= 57 && e.keyCode >= 48) ||
+      (e.keyCode >= 65 && e.keyCode <= 90)
+    ) {
+      props.onKeyUp();
+    } else {
+      ref.current.value = '';
+    }
+  }
   return (
     <input
       type="text"
       max={9}
       min={0}
       required={true}
-      onKeyUp={() => props.onKeyUp()}
+      onKeyUp={(e) => keyUp(e)}
       className="bg-gray-100 p-3 px-0 text-xl m-0.5 text-center border-2 rounded focus:ring-2 focus:ring-offset-2"
       ref={ref}
       size={1}
@@ -43,11 +53,9 @@ function Inputs(props: {
 }
 export default function DigitInputs(props: { setDigit?: Function }) {
   // 現在入力済みのコード
-  const [digit, setDigit] = useState<number[]>([0, 0, 0, 0, 0, 0]);
+  const [digit, setDigit] = useState<string[]>(['', '', '', '', '', '']);
   // 現在フォーカスされている桁
-  const [focusedElement, setFocusedElement] = useState<number>(
-    Math.min(...digit)
-  );
+  const [focusedElement, setFocusedElement] = useState<number>(0);
   const elementsIds: number[] = [0, 1, 2, 3, 4, 5, 6];
   function elementFocus(id) {
     console.log(id);
--- pages/playground.tsx
+++ pages/playground.tsx
@@ -1,13 +1,15 @@
 import Add_event_form from '../components/Add_event_form';
 import { WrapperProvider } from './../contexts/Wrapper';
 import DigitInput from './../components/DigitInput';
+import { useState } from 'react';
 
 export default function Playground() {
+  const [digit, setDigit] = useState<string>('')
   return (
     <>
       <WrapperProvider>
         <h1>Playground</h1>
-        <DigitInput />
+        <DigitInput setDigit={setDigit}/>
       </WrapperProvider>
     </>
   );

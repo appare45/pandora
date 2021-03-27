293469ad5d78edc9d2092b0a0cce0dce0d19e568
appare45 <kaibatsu35.7m45@gmail.com>
2021-03-27 13:01:05 +0900

    Event infoのスマホ向けスタイルが完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,7 @@ export function AddEvent() {
   );
 }
 
+// イベントの情報
 const EventInfo = React.memo((props: { userData: DocumentData }) => {
   const context = useContext(AuthContext);
   const db: FirebaseFirestore = context.data;
@@ -40,8 +41,9 @@ const EventInfo = React.memo((props: { userData: DocumentData }) => {
     }
   }, [props.userData]);
   return (
-    <div className="bg-blue-50 p-5 flex flex-col items-center justify-center">
+    <form className="bg-blue-50 p-5 flex flex-col items-center justify-center">
       <div className="w-44 h-44 bg-gray-50 rounded-full shadow-inner relative">
+        {/* アイコン画像 */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -62,6 +64,7 @@ const EventInfo = React.memo((props: { userData: DocumentData }) => {
             className="w-full h-full absolute top-0 left-0"
           />
         )}
+        {/* 変更ボタン */}
         <button className="bg-gray-50 border text-gray-600 border-gray-200 absolute bottom-1 right-1 w-9 h-9 p-1.5 rounded-full">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -79,13 +82,69 @@ const EventInfo = React.memo((props: { userData: DocumentData }) => {
         </button>
       </div>
       <div className="md:p-10 py-5 flex-1">
-        <p className="text-lg">イベント名：{currentEvent?.name}</p>
-        <p className="text-lg">説明：{currentEvent?.description}</p>
+        <div className="mb-2">
+          <label htmlFor="eventTitle" className="text-xs text-gray-500">
+            イベント名
+          </label>
+          <div className="relative">
+            <input
+              type="text"
+              id="eventTitle"
+              value={currentEvent?.name}
+              className="p-0.5 bg-transparent border-b-2 w-full"
+            />
+            <button className="absolute right-0 bottom-0 w-7 h-7 p-1 text-gray-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="eventDescription" className="text-xs text-gray-500">
+            説明
+          </label>
+          <div className="relative">
+            <textarea
+              cols={30}
+              rows={5}
+              id="eventDescription"
+              value={currentEvent?.description}
+              className="p-0.5 bg-transparent border-2"
+            ></textarea>
+            <button className="absolute right-1 bottom-2 w-7 h-7 p-1 text-gray-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </form>
   );
 });
 
+// 表示するメインの部分
 const App = React.memo((props: { user: firebase.User }) => {
   const context = useContext(AuthContext);
   const db: FirebaseFirestore = context.data;
